420b47bf89d0f7488cb622e5934ced548c2a1154
3DG4RDC26 <ercasco@uamv.edu.ni>
2024-09-23 08:52:36 -0600

    Generacion del Forms.cs

--- Corte1/Corte1/Form1.cs
+++ Corte1/Corte1/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Corte1.clases;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,58 @@ namespace Corte1
 {
    public partial class Form1 : Form
    {
+      private Registro registro;
+      private Operacion operacion;
+
       public Form1()
       {
          InitializeComponent();
+         registro = new Registro();
+         operacion = new Operacion();
+         cmbCiudad.Items.AddRange(new string[] { "Ciudad1", "Ciudad2", "Ciudad3" });
+      }
+
+      private void buttonAgregar_Click(object sender, EventArgs e)
+      {
+         string nombres = tbName.Text;
+         string apellidos = tbLastName.Text;
+         DateTime fechaNacimiento = dtpFechaNac.Value;
+
+         if (cmbCiudad.SelectedItem == null)
+         {
+            MessageBox.Show("Por favor, seleccione una ciudad.");
+            return;
+         }
+
+         string ciudad = cmbCiudad.SelectedItem.ToString();
+         Persona nuevaPersona = new Persona(nombres, apellidos, fechaNacimiento, ciudad);
+
+         try
+         {
+            registro.AgregarPersona(nuevaPersona);
+            lbPersona.Items.Add($"{nombres} {apellidos}, {ciudad}");
+            MessageBox.Show("Persona agregada exitosamente.");
+         }
+         catch (Exception ex)
+         {
+            MessageBox.Show(ex.Message);
+         }
+      }
+
+      private void buttonMostrarEdad_Click(object sender, EventArgs e)
+      {
+         if (registro.ObtenerPersonas().Count > 0)
+         {
+            Persona persona = registro.ObtenerPersonas()[0];
+            int edad = operacion.CalcularEdad(persona.FechaNacimiento);
+            string mayorEdad = operacion.VerificarMayorEdad(persona.FechaNacimiento);
+
+            MessageBox.Show($"Edad: {edad} años. {mayorEdad}");
+         }
+         else
+         {
+            MessageBox.Show("No hay personas registradas.");
+         }
       }
    }
 }
