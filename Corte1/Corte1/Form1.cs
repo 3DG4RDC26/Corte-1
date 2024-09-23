825c8fb7a86e9accc9751202cec70d8f6e14f2b8
3DG4RDC26 <ercasco@uamv.edu.ni>
2024-09-23 09:57:20 -0600

    Ultimo commit

--- Corte1/Corte1/Form1.cs
+++ Corte1/Corte1/Form1.cs
@@ -11,60 +11,60 @@ using System.Windows.Forms;
 
 namespace Corte1
 {
-   public partial class Form1 : Form
-   {
-      private Registro registro;
-      private Operacion operacion;
+    public partial class Form1 : Form
+    {
+        private Registro registro;
+        private Operacion operacion;
 
-      public Form1()
-      {
-         InitializeComponent();
-         registro = new Registro();
-         operacion = new Operacion();
-         cmbCiudad.Items.AddRange(new string[] { "Ciudad1", "Ciudad2", "Ciudad3" });
-      }
+        public Form1()
+        {
+            InitializeComponent();
+            registro = new Registro();
+            operacion = new Operacion();
+            cmbCiudad.Items.AddRange(new string[] { "Managua", "Chinandega", "Esteli" });
+        }
 
-      private void buttonAgregar_Click(object sender, EventArgs e)
-      {
-         string nombres = tbName.Text;
-         string apellidos = tbLastName.Text;
-         DateTime fechaNacimiento = dtpFechaNac.Value;
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            string nombres = tbName.Text;
+            string apellidos = tbLastName.Text;
+            DateTime fechaNacimiento = dtpFechaNac.Value;
 
-         if (cmbCiudad.SelectedItem == null)
-         {
-            MessageBox.Show("Por favor, seleccione una ciudad.");
-            return;
-         }
+            if (cmbCiudad.SelectedItem == null)
+            {
+                MessageBox.Show("Por favor, seleccione una ciudad.");
+                return;
+            }
 
-         string ciudad = cmbCiudad.SelectedItem.ToString();
-         Persona nuevaPersona = new Persona(nombres, apellidos, fechaNacimiento, ciudad);
+            string ciudad = cmbCiudad.SelectedItem.ToString();
+            Persona nuevaPersona = new Persona(nombres, apellidos, fechaNacimiento, ciudad);
 
-         try
-         {
-            registro.AgregarPersona(nuevaPersona);
-            lbPersona.Items.Add($"{nombres} {apellidos}, {ciudad}");
-            MessageBox.Show("Persona agregada exitosamente.");
-         }
-         catch (Exception ex)
-         {
-            MessageBox.Show(ex.Message);
-         }
-      }
+            try
+            {
+                registro.AgregarPersona(nuevaPersona);
+                lbPersona.Items.Add($"{nombres} {apellidos}, {ciudad}");
+                MessageBox.Show("Persona agregada exitosamente.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
 
-      private void buttonMostrarEdad_Click(object sender, EventArgs e)
-      {
-         if (registro.ObtenerPersonas().Count > 0)
-         {
-            Persona persona = registro.ObtenerPersonas()[0];
-            int edad = operacion.CalcularEdad(persona.FechaNacimiento);
-            string mayorEdad = operacion.VerificarMayorEdad(persona.FechaNacimiento);
+        private void btnMostrar_Click_1(object sender, EventArgs e)
+        {
+            if (registro.ObtenerPersonas().Count > 0)
+            {
+                Persona persona = registro.ObtenerPersonas()[0];
+                int edad = operacion.CalcularEdad(persona.FechaNacimiento);
+                string mayorEdad = operacion.VerificarMayorEdad(persona.FechaNacimiento);
 
-            MessageBox.Show($"Edad: {edad} años. {mayorEdad}");
-         }
-         else
-         {
-            MessageBox.Show("No hay personas registradas.");
-         }
-      }
-   }
+                MessageBox.Show($"Edad: {edad} años. {mayorEdad}");
+            }
+            else
+            {
+                MessageBox.Show("No hay personas registradas.");
+            }
+        }
+    }
 }
